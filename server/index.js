fdae8e3c4ef8db0e7d18bf17e04f8ecef2c93b36
jamesj94 <jhrayj@gmail.com>
2018-01-29 23:30:30 -0800

    Setup index.js with express and to use morgan/bodyParser. Also installed nodemon package to simplify server restart

--- server/index.js
+++ server/index.js
@@ -1,1 +1,27 @@
-//Main starting point of the applicatin.
+//Main starting point of the application.
+const express    = require('express');
+const http       = require('http');
+//bodyParser and morgan are express middleware
+//bodyParser parses incoming requests -> specifically json
+const bodyParser = require('body-parser');
+//morgan is a logging framework.
+const morgan     = require('morgan');
+
+const app = express();
+
+//App setup
+//Getting express to talk to the world
+//*use* registers packages as middleware.
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
+
+
+//Server Setup
+const port = process.envPORT || 3090;
+
+//create an http server that knows
+//how to receive requests. We will add
+//functionality to app.
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
